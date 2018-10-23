61e1be91221db64cc51f077ffbeec18d2d6c7a44
omer9823 <omer37374@gmail.com>
2018-10-23 15:17:33 +0300

    secound

--- app/src/main/java/com/example/user/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/user/alertdialog/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.user.alertdialog;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout LL1;
     Button btn1;
     Button btn2;
     Button btn3;
     Button btn4;
     Button btn5;
     AlertDialog.Builder adb;
-
+    int r,g,b;
+    Random rnd = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,35 +32,111 @@ public class MainActivity extends AppCompatActivity {
         btn3 = (Button) findViewById(R.id.btn3);
         btn4 = (Button) findViewById(R.id.btn4);
         btn5 = (Button) findViewById(R.id.btn5);
+        LL1 = (LinearLayout) findViewById(R.id.LL1);
 
 
     }
 
     public void ome1(View view) {
 
-        adb=new AlertDialog.Builder(this);
+        adb = new AlertDialog.Builder(this);
 
         adb.setTitle("hello");
         adb.setMessage("only text");
 
-        AlertDialog ad=adb.create();
+        AlertDialog ad = adb.create();
         ad.show();
     }
 
-    public void ome2(View view){
+    public void ome2(View view) {
 
-        adb=new AlertDialog.Builder(this);
+        adb = new AlertDialog.Builder(this);
 
         adb.setTitle("hello");
         adb.setMessage("only text");
-
-        AlertDialog ad=adb.create();
+        adb.setIcon(R.drawable.hamster1);
+        AlertDialog ad = adb.create();
         ad.show();
-
 
     }
 
+    public void ome3(View view) {
+
+        adb = new AlertDialog.Builder(this);
+
+        adb.setTitle("hello");
+        adb.setMessage("only text");
+        adb.setIcon(R.drawable.hamster1);
+        adb.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog ad = adb.create();
+        ad.show();
+    }
+
+    public void ome4(View view) {
+
+        r=rnd.nextInt(255);
+        g=rnd.nextInt(255);
+        b=rnd.nextInt(255);
+
+        adb.setTitle("change the background");
+        adb.setMessage("you want to change the color?");
+        adb.setIcon(R.drawable.hamster1);
+        adb.setPositiveButton("change color", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                LL1.setBackgroundColor(Color.rgb(r,g,b));
 
 
+            }
+        });
+        adb.setNegativeButton("close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
+            }
+        });
+        AlertDialog ad = adb.create();
+        ad.show();
+    }
+
+    public void ome5(View view){
+        r=rnd.nextInt(255);
+        g=rnd.nextInt(255);
+        b=rnd.nextInt(255);
+
+        adb.setTitle("change the background");
+        adb.setMessage("you want to change the color?");
+        adb.setIcon(R.drawable.hamster1);
+        adb.setPositiveButton("change color", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                LL1.setBackgroundColor(Color.rgb(r,g,b));
+
+
+            }
+        });
+        adb.setNeutralButton("rest", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                LL1.setBackgroundColor(Color.WHITE);
+
+            }
+        });
+        adb.setNegativeButton("close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog ad = adb.create();
+        ad.show();
+
+    }
 }
+
